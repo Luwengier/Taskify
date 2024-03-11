3dcaf637a4a939df85b5b8196cf718165ff4a8a6
Wengier <g217881111@gmail.com>
2024-03-11 18:27:43 +0800

    Build nav item boilerplate of dashboard group

---
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -0,0 +1,19 @@
+'use-client';
+
+export type Organization = {
+  id: string;
+  name: string;
+  slug: string;
+  imageUrl: string;
+};
+
+interface NavItemProps {
+  isExpanded: boolean;
+  isActive: boolean;
+  organization: Organization;
+  onExpand: (id: string) => void;
+}
+
+export const NavItem = (props: NavItemProps) => {
+  return <div>Nav Item</div>;
+};
