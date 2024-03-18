5b260781947e66f48f33b275baa0d9b95ebbae68
Luewngier <g217881111@gmail.com>
2024-03-18 14:12:48 +0800

    Add validation to create-board action of dashboard group

--- actions/create-board.ts
+++ actions/create-board.ts
@@ -1,13 +1,23 @@
 'use server';
 
+import { z } from 'zod';
 import { db } from '@/lib/db';
+import { revalidatePath } from 'next/cache';
+
+const CreateBoard = z.object({
+  title: z.string(),
+});
 
 export async function create(formData: FormData) {
-  const title = formData.get('title') as string;
+  const { title } = CreateBoard.parse({
+    title: formData.get('title') as string,
+  });
 
   await db.board.create({
     data: {
       title,
     },
   });
+
+  revalidatePath('/organization/org_2dUYcWipjvtMGpORgrObQn2s6m8');
 }
