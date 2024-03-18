aeb31becb12177c4f7165bbcbb582434299e68ae
Luewngier <g217881111@gmail.com>
2024-03-18 14:13:39 +0800

    Create delete-board action to dashboard

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,9 +1,14 @@
 import { create } from '@/actions/create-board';
 import { Button } from '@/components/ui/button';
+import { db } from '@/lib/db';
 
-const OrganizationIdPage = () => {
+import Board from './board';
+
+const OrganizationIdPage = async () => {
+  const boards = await db.board.findMany();
+
   return (
-    <div>
+    <div className="flex flex-col space-y-4">
       <form action={create}>
         <input
           type="text"
@@ -16,6 +21,12 @@ const OrganizationIdPage = () => {
 
         <Button type="submit">Submit</Button>
       </form>
+
+      <div className="space-y-2">
+        {boards.map((board) => (
+          <Board key={board.id} title={board.title} id={board.id} />
+        ))}
+      </div>
     </div>
   );
 };
