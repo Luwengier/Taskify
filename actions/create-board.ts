055bfcb92c7606674b7910a2124e502f604a6c97
Luewngier <g217881111@gmail.com>
2024-03-23 13:10:47 +0800

    Add error status to create-board action

--- actions/create-board.ts
+++ actions/create-board.ts
@@ -1,23 +1,50 @@
 'use server';
 
 import { z } from 'zod';
-import { db } from '@/lib/db';
 import { revalidatePath } from 'next/cache';
 
+import { db } from '@/lib/db';
+import { redirect } from 'next/navigation';
+
+export type State = {
+  errors?: {
+    title?: string[];
+  };
+  message?: string | null;
+};
+
 const CreateBoard = z.object({
-  title: z.string(),
+  title: z.string().min(3, {
+    message: 'Minimum length of 3 letters is required',
+  }),
 });
 
-export async function create(formData: FormData) {
-  const { title } = CreateBoard.parse({
-    title: formData.get('title') as string,
+export async function create(prevState: State, formData: FormData) {
+  const validateFields = CreateBoard.safeParse({
+    title: formData.get('title'),
   });
 
-  await db.board.create({
-    data: {
-      title,
-    },
-  });
+  if (!validateFields.success) {
+    return {
+      errors: validateFields.error.flatten().fieldErrors,
+      message: 'Missing fields.',
+    };
+  }
+
+  const { title } = validateFields.data;
+
+  try {
+    await db.board.create({
+      data: {
+        title,
+      },
+    });
+  } catch (error) {
+    return {
+      message: 'Database Error',
+    };
+  }
 
   revalidatePath('/organization/org_2dUYcWipjvtMGpORgrObQn2s6m8');
+  redirect('/organization/org_2dUYcWipjvtMGpORgrObQn2s6m8');
 }
