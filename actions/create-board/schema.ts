ab94b619ef1e0bf01b469ef1cd1f6bf5658014b4
Luewngier <g217881111@gmail.com>
2024-04-03 13:15:05 +0800

    Fix error initialize issue in use-action

--- actions/create-board/schema.ts
+++ actions/create-board/schema.ts
@@ -3,8 +3,8 @@ import { z } from 'zod';
 export const CreateBoard = z.object({
   title: z
     .string({
-      required_error: 'Title is required',
-      invalid_type_error: 'Title must be a string',
+      required_error: 'Title is required.',
+      invalid_type_error: 'Title is required.',
     })
     .min(3, {
       message: 'Title is too short',
